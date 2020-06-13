45e3c0d47a9cdc72e78da6cb5577bf10dba4cdaa
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-06-13 22:36:38 +0800

    remove face landmarks and recognition

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const webcam = document.getElementById('webcam')
 
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+//   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+//   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+//   faceapi.nets.faceExpressionNet.loadFromUri('/models')
+// ]).then(startVideo)
+faceapi.nets.tinyFaceDetector.loadFromUri('/models').then(startVideo)
 
 function startVideo() {
   navigator.mediaDevices.getUserMedia({ video: {} })
@@ -16,14 +17,15 @@ function startVideo() {
 webcam.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(webcam)
   document.body.append(canvas)
-  const displaySize = { width: webcam.videoWidth, height: webcam.videoHeight }
+  const displaySize = { width: webcam.clientWidth, height: webcam.clientHeight }
   faceapi.matchDimensions(canvas, displaySize)
+
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(webcam, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectSingleFace(webcam, new faceapi.TinyFaceDetectorOptions())//.withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
